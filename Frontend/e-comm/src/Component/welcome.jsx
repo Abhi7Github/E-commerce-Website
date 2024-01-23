3c9e49fb132512fb166a8a6d9abfa7152190b5cf
Abhijeet <abhijeetmali.it@gmail.com>
2024-01-23 21:01:35 +0530

    E-commerce

---
+++ Frontend/e-comm/src/Component/welcome.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Welcome = () => {
+    return (
+        <>
+            <div className="welcome" style={{height:"35rem"}}>
+                <div className="h1 container text-center my-0 ">WELCOME</div>
+                <div className="h4 container text-center my-2">TO THE</div>
+                <div className="h1 container text-center my-2 ">A-MART</div>
+            </div>
+        </>
+    )
+}
